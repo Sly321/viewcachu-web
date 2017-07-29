b7f9f4764f279a636e886adf0af6d7e17f8ecd09
Sly321 <liebigsv@gmail.com>
2017-07-29 21:36:29 +0200

    implement dummy methods

--- src/app/models/series.ts
+++ src/app/models/series.ts
@@ -1,4 +1,4 @@
-export class Serie {
+export class Series {
 	/**
 	 * Image path.
 	 *
--- src/app/services/firebase/firebase.service.ts
+++ src/app/services/firebase/firebase.service.ts
@@ -1,10 +1,36 @@
 import { Injectable } from '@angular/core';
 import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+import { Authentification } from '../../services/authentification/authentification.service';
+
+import { Series } from '../../models/series';
 
 @Injectable()
 export class FirebaseService {
 
-	constructor(db: AngularFireDatabase) {
+	constructor(private db: AngularFireDatabase, private auth: Authentification) {
 	}
 
+	addSeriesToUser(series: Series) {
+		console.log('not implemented yet');
+	}
+
+	checkEpisodeOfSeries(seriesid: number, season: number, episode: number) {
+		console.log('not implemented yet');
+	}
+
+	hasUserDb(): boolean {
+		const uid: string = this.auth.getUser().uid;
+		console.log(`uid: ${uid}`);
+		if (uid) {
+			const db = this.db.list('/' + uid);
+			console.log(`uid: ${db}`);
+			if (db) {
+				return true;
+			} else {
+				return false;
+			}
+		} else {
+			return false;
+		}
+	}
 }
