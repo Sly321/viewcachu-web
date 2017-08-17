0596a39574cfaa4dc02741de106b37ad15d771cc
Sven Liebig <s.liebig@schuetze-consulting.ag>
2017-08-17 16:52:55 +0200

    update content test

--- src/app/components/content/content.component.spec.ts
+++ src/app/components/content/content.component.spec.ts
@@ -1,25 +1,38 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { Component } from '@angular/core';
 
 import { ContentComponent } from './content.component';
 
+@Component({
+	selector: 'router-outlet',
+	template: '<h1>mockingjay</h1>'
+})
+class RouterMock { }
+
+class MockRouter { public navigate() { } }
+export const routes = [
+	{ path: '', component: RouterMock },
+	{ path: '**', component: RouterMock },
+];
+
 describe('ContentComponent', () => {
-  let component: ContentComponent;
-  let fixture: ComponentFixture<ContentComponent>;
+	let component: ContentComponent;
+	let fixture: ComponentFixture<ContentComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ ContentComponent ]
-    })
-    .compileComponents();
-  }));
+	beforeEach(async(() => {
+		TestBed.configureTestingModule({
+			declarations: [ ContentComponent, RouterMock ]
+		})
+		.compileComponents();
+	}));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(ContentComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+	beforeEach(() => {
+		fixture = TestBed.createComponent(ContentComponent);
+		component = fixture.componentInstance;
+		fixture.detectChanges();
+	});
 
-  it('should be created', () => {
-    expect(component).toBeTruthy();
-  });
+	it('should be created', () => {
+		expect(component).toBeTruthy();
+	});
 });
