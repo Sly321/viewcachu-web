3086dcb2533e7eea214a1dcf33366b8d64f78beb
Sven Liebig <s.liebig@schuetze-consulting.ag>
2017-08-17 16:53:41 +0200

    update search-result test

--- src/app/components/search-result/search-result.component.spec.ts
+++ src/app/components/search-result/search-result.component.spec.ts
@@ -1,25 +1,27 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SearchResultComponent } from './search-result.component';
+import { SeriesInfoResponseByName } from '../../models/seriesInfoResponseByName';
 
 describe('SearchResultComponent', () => {
-  let component: SearchResultComponent;
-  let fixture: ComponentFixture<SearchResultComponent>;
+	let component: SearchResultComponent;
+	let fixture: ComponentFixture<SearchResultComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ SearchResultComponent ]
-    })
-    .compileComponents();
-  }));
+	beforeEach(async(() => {
+		TestBed.configureTestingModule({
+			declarations: [ SearchResultComponent ]
+		})
+		.compileComponents();
+	}));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(SearchResultComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+	beforeEach(() => {
+		fixture = TestBed.createComponent(SearchResultComponent);
+		component = fixture.componentInstance;
+		component.serie = new SeriesInfoResponseByName();
+		fixture.detectChanges();
+	});
 
-  it('should be created', () => {
-    expect(component).toBeTruthy();
-  });
+	it('should be created', () => {
+		expect(component).toBeTruthy();
+	});
 });
