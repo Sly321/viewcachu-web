48bd545dcb60755161f3200493e368fc9087cc79
Sly321 <liebigsv@gmail.com>
2017-08-24 22:34:05 +0200

    add styles

--- src/app/components/board/board.component.ts
+++ src/app/components/board/board.component.ts
@@ -6,7 +6,7 @@ import { Series } from '../../models/series';
 @Component({
 	selector: 'app-board',
 	templateUrl: './board.component.html',
-	styleUrls: ['./board.component.css']
+	styleUrls: ['./board.component.less']
 })
 export class BoardComponent implements OnInit {
 	private list: Array<Series>;
@@ -26,12 +26,17 @@ export class BoardComponent implements OnInit {
 		this.fb.getUserSeries((array: Array<Series>) => {
 			this.list = new Array<Series>();
 			this.loading = true;
+			const waitfor = array.length;
+			let counter = 0;
 			array.forEach((series: Series) => {
 				this.fb.getSeries(series.$id, (seriesResult: Series) => {
 					this.list.push(seriesResult);
+					counter++;
+					if (counter === waitfor) {
+						this.loading = false;
+					}
 				});
 			});
-			this.loading = false;
 		});
 	}
 }
